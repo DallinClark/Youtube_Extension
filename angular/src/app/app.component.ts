5a4202d90c89b4896486543065711d68a497ad20
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-03-08 21:31:13 -0700

    get the current youtube video id

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+declare const chrome: any;
 
 interface Comment {
   author: string;
@@ -15,8 +16,28 @@ interface Comment {
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+
+  currentTabUrl: string = 'loading...';
+
+  constructor(private cdRef: ChangeDetectorRef) { }
+
+  getYoutubeVideoId(url: string): string {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.{11})/;
+    const match = url.match(regex);
+    console.log(match);
+    return match && match[1].length === 11 ? match[1] : 'Not a valid YouTube URL.';
+  };
+
+  ngOnInit() {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs: any) => {
+      if (tabs.length > 0) {
+        this.currentTabUrl = this.getYoutubeVideoId(tabs[0].url);
+        this.cdRef.detectChanges();
+      }
+    });
+  }
 
   comments: Comment[] = [
     {
