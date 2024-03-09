d1d99e676bcd5b0e4157d4f2283b924d76ef9a20
rencherga <rencher.grant@gmail.com>
2024-03-08 17:43:33 -0700

    formatting

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 
 async function getComments(videoId, apiKey) {
+
+    let raw_comments = []
+
     try {
         const response = await axios.get(`https://www.googleapis.com/youtube/v3/commentThreads`, {
             params: {
@@ -13,24 +16,32 @@ async function getComments(videoId, apiKey) {
 
         const comments = response.data.items;
 
-        for (const comment of comments) {
-            const commentText = comment.snippet.topLevelComment.snippet.textDisplay;
-            console.log(`Comment: ${commentText}`);
+        // console.log(comments)
 
-            if (comment.replies) {
-                for (const reply of comment.replies.comments) {
-                    const replyText = reply.snippet.textDisplay;
-                    console.log(`Reply: ${replyText}`);
-                }
-            }
+        for (const comment of comments) {
+            // const commentText = comment.snippet.topLevelComment.snippet.textDisplay;
+            // console.log(`Comment: ${commentText}`);
+
+            // console.log(comment.snippet.topLevelComment)
+
+            raw_comments.push(comment.snippet.topLevelComment.snippet.textOriginal)
+
+            // if (comment.replies) {
+            //     for (const reply of comment.replies.comments) {
+            //         const replyText = reply.snippet.textDisplay;
+            //         console.log(`Reply: ${replyText}`);
+            //     }
+            // }
         }
     } catch (error) {
         console.error(`Error fetching comments: ${error}`);
     }
+
+    console.log(raw_comments)
 }
 
 // Replace 'YOUR_API_KEY' with your actual YouTube Data API key
-const API_KEY = 'AIzaSyCCZ7zfyqQhm-Bh43N4XbrGSqNO-4si8Ug';
+const API_KEY = 'AIzaSyADXH44QlQYsv6jiTEC7L-7DQUwrP-m618';
 
 // Replace 'VIDEO_ID' with the ID of the YouTube video you want to scrape comments from
 const VIDEO_ID = '0FtcHjI5lmw';
