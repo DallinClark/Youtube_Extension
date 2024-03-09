4e9b1cd16ddca52a711e5d806af2c9e35493edba
peterhu72 <peterhu4000@gmail.com>
2024-03-08 21:04:25 -0700

    real comments

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import axios from 'axios';
 
 interface Comment {
   author: string;
@@ -15,24 +16,31 @@ interface Comment {
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  key = "AIzaSyCgkOGTu8drhwATKnA4y-gVhu6-3O1FO4w"
+  comments: Comment[] = []
+  async ngOnInit() {
+    alert("hi")
+    console.log("hi")
+    await this.getComments()
+  }
 
-  comments: Comment[] = [
-    {
-      author: 'John Doe',
-      date: '2023-03-08',
-      text: 'This is an awesome video!'
-    },
-    {
-      author: 'Jane Smith',
-      date: '2023-03-07',
-      text: 'I really enjoyed watching this tutorial.'
-    },
-    {
-      author: 'Bob Johnson',
-      date: '2023-03-06',
-      text: 'Great content! Keep up the good work.'
+
+  async getComments() {
+    const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&order=relevance&textFormat=plainText&videoId=zeD0g5xXo7E&key=${this.key}`)
+    console.log(res)
+    const items = res.data.items
+    for (let i = 0; i < items.length; i++) {
+      const comment = {
+        author: items[i].snippet.topLevelComment.snippet.authorDisplayName,
+        date: items[i].snippet.topLevelComment.snippet.publishedAt,
+        text: items[i].snippet.topLevelComment.snippet.textDisplay
+      }
+      this.comments.push(comment)
     }
-  ];
+    console.log(this.comments)
+  }
+
+  
 }
