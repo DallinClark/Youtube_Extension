43cc29c592b442cc5e55e3b51f6a856870176daf
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-03-08 19:22:25 -0700

    popup now loads properly

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -10,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    console.log('AppComponent constructor');
+  }
+
+  onButtonClick() {
+    console.log('Button clicked');
+  }
 }
